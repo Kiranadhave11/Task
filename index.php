bf5ec667478c0292388ceb849304438e05e89279
Kiranadhave11 <60655027+Kiranadhave11@users.noreply.github.com>
2021-06-05 17:10:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,362 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Task</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+    <link href="https://fonts.googleapis.com/css?family=Rubik:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/rangeslider.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+    
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+    
+    <header class="site-navbar" role="banner">
+
+      <div class="container">
+        <div class="row align-items-center">
+          
+          <div class="col-11 col-xl-4">
+            <h1 class="mb-0 site-logo"><a href="index.php" class="text-white mb-0">Nasscorp</a></h1>
+          </div>
+          <div class="col-12 col-md-8 d-none d-xl-block">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+
+              <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block">
+                <li class="active"><a href="index.php"><span>home</span></a></li>
+                <li><a href=""><span>product</span></a></li>
+                <li><a href=""><span>pricing</span></a></li>
+                <li><a href=""><span>contact</span></a></li>
+                <li class="nav-item"> <i class="fas fa-shopping-cart pl-0"></i></li>
+
+                <i class="fas fa-search"></i>
+              </ul>
+            </nav>
+          </div>
+
+
+          <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
+
+          </div>
+
+        </div>
+      </div>
+    </header>
+
+
+
+  
+
+    <div class="site-blocks-cover overlay" style="background-image: url(images/bg.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+      <div class="container">
+        <div class="row align-items-center justify-content-center text-center">
+
+          <div class="col-md-10">
+            
+            <div class="row justify-content-center mb-4">
+              <div class="col-md-10 text-center">
+                <h1 data-aos="fade-up" class="mb-5">Drive More Customers Through Digital</h1>
+                <p class="text">We know how large objects will act,but things on a small scale just do not act that way.</p>
+
+                <p data-aos="fade-up" data-aos-delay="100"><a href="#" class="btn btn-primary btn-pill" style="background: #FF8C00;border: none;">get quote now</a>
+                 <a href="#" class="btn btn-primary btn-pill" style="background: transparent; border-color: white;text-transform: capitalize;" >learn more</a>
+                </p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>  
+
+
+
+    <div class="block-quick-info-2">
+      <div class="container">
+        <div class="block-quick-info-2-inner">
+          <div class="row">
+            <div class="col-sm-6 col-md-6 col-lg-3 mb-3 mb-lg-0">
+              <div class="d-flex quick-info-2">
+                <span class="icon icon-home mr-3"></span>
+                <div class="text">
+                  <strong class="d-block heading">1.5K</strong>
+                  <span class="excerpt">happy<br> customers</span>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-3 mb-3 mb-lg-0">
+              <div class="d-flex quick-info-2">
+                <span class="icon icon-phone mr-3"></span>
+                <div class="text">
+                  <strong class="d-block heading">3K</strong>
+                  <span class="excerpt">cases done</span>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-3 mb-3 mb-lg-0">
+              <div class="d-flex quick-info-2">
+                <span class="icon icon-envelope mr-3"></span>
+                <div class="text">
+                  <strong class="d-block heading">45</strong>
+                  <span class="excerpt">award winning</span>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-3 mb-3 mb-lg-0">
+              <div class="d-flex quick-info-2">
+                <span class="icon icon-clock-o mr-3"></span>
+                <div class="text">
+                  <strong class="d-block heading">12+</strong>
+                  <span class="excerpt">countries worldwide</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <section>
+    <div class="container mt-5">
+    <div class="row">
+        <div class="col-lg-3  ">
+            <div class="card">
+                <div class="card-block">
+                  <div class="d-flex quick-info-2">
+                  <span class="icon icon-home mr-3"></span>
+                  </div>
+                    <p class="card-title">business growing</p>
+                    <p class="card-text">the quick for jump over the lazy dog</p>
+                   <a href="#" class="btn btn-primary btn-pill" style="background: white; border-color: #009FFF; color: #009FFF;" >More</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+             <div class="card">
+                <div class="card-block">
+                   <div class="d-flex quick-info-2">
+                  <span class="icon icon-home mr-3"></span>
+                  </div>
+                    <p class="card-title">digital marketing</p>
+                    <p class="card-text">the quick for jump over the lazy dog</p>
+                   <a href="#" class="btn btn-primary btn-pill" style="background: white; border-color: #009FFF; color: #009FFF;" >More</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+             <div class="card">
+                <div class="card-block">
+                   <div class="d-flex quick-info-2">
+                  <span class="icon icon-home mr-3"></span>
+                  </div>
+                    <p class="card-title">national top 50 firms</p>
+                    <p class="card-text">the quick for jump over the lazy dog</p>
+                   <a href="#" class="btn btn-primary btn-pill" style="background:white; border-color: #009FFF; color: #009FFF;" >More</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+             <div class="card">
+                <div class="card-block">
+                   <div class="d-flex quick-info-2">
+                  <span class="icon icon-home mr-3"></span>
+                  </div>
+                    <p class="card-title">digital marketing</p>
+                    <p class="card-text">the quick for jump over the lazy dog</p>
+                   <a href="#" class="btn btn-primary btn-pill" style="background: white; border-color: #009FFF; color: #009FFF;" >More</a>
+                </div>
+            </div>
+        </div>   
+    </div>
+</div>
+</section>
+<section>
+  <div class="mt-5">
+      <h1 class="text1">We are providing best business service.</h1>
+      <p class="text5">problems trying to resolve the conflict between the two major realms of Classical physics:Newtonian mechanics</p>
+      <div class="row">
+        <div class="col-lg-6">
+          <video>
+
+          </video>
+        </div>
+        <div class="col-lg-6">
+          <h1 class="text2">Most  trusted in our field</h1>
+          <p class="text9">Most calendars are designed for  teams.Slate is designed for freelancers who want a simple way to plan their schedule.</p>
+          <ul>
+            <li>
+
+              <p class="text10">the quick for jump over the lazy dog
+              Things on a very small scale...</p>
+            </li>
+            <li>
+              <p class="text10">the quick for jump over the lazy dog
+                Things on a very small scale...</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+</section>
+</div>
+<section class="clients" >
+  <div class="container ">
+      <h4 class="text3 ">What Clients Say</h4>
+      <p class=text5>problems trying to resolve the conflict between the two major realms of Classical physics:Newtonian mechanics</p>
+      
+   </div>
+</section>
+<section>
+<div class="mt-5">
+    <h4 class="text3">Meet Our Team</h4>
+    <p class="text5">problems trying to resolve the conflict between the two major realms of Classical physics:Newtonian mechanics</p>
+  <div class="page-wrapper">
+    <div class="profile-box">
+      <img src="https://placeimg.com/200/200/any" alt="profile pic">
+      <p class="text6">consultant</p>
+      <p class="card-title">rodney stratton</p>
+        <p>the quick for jump over the lazy dog</p>
+    </div>
+    <div class="profile-box">
+      <img src="https://placeimg.com/200/200/animal" alt="profile pic">
+        <p class="text6">consultant</p>
+       <p class="card-title">ben jonson</p>
+        <p>the quick for jump over the lazy dog</p>
+      
+    </div>
+    <div class="profile-box">
+      <img src="https://placeimg.com/200/200/arch" alt="profile pic">
+        <p class="text6">consultant</p>
+           <p class="card-title">rodney stratton</p>
+         <p>the quick for jump over the lazy dog</p>
+      
+    </div>
+    <div class="profile-box">
+      <img src="https://placeimg.com/200/200/nature" alt="profile pic">
+        <p class="text6">co founder</p>
+       <p class="card-title">avie beaton</p>
+        <p>the quick for jump over the lazy dog</p>
+     
+    </div>
+  </div>
+  </div>
+
+ <div class="row container">
+    <div class="col-lg-6">
+      <h1 class="text4">we have brunches all over the world</h1>
+      <p class="text8">The gradual accumulation of information about atomic and small-scale behaviour during the first quarter of the 20th century,which gave some indictions about how small things do behave,produced an increasing confusion which was Heisenberg,and Born.  </p>
+    </div>
+    <div class="col-lg-6">
+      
+    </div>
+  </div>
+  </div>
+</div></section><br>
+
+ <!-- Footer -->
+  <footer style="background: #0f0c29;">
+    <div class="container py-5">
+      <div class="row py-4">
+        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+          <h6 class="text-uppercase font-weight-bold mb-4">get in touch</h6>
+          <p class="text7 ">the quick for jump over the lazy dog</p>
+          <ul class="list-inline mt-4">
+            <li class="list-inline-item"><a href="#" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#" target="_blank" title="facebook"><i class="fa fa-facebook"></i></a></li>
+            <li class="list-inline-item"><a href="#" target="_blank" title="instagram"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" target="_blank" title="pinterest"><i class="fa fa-pinterest"></i></a></li>
+            <li class="list-inline-item"><a href="#" target="_blank" title="vimeo"><i class="fa fa-vimeo"></i></a></li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+          <h6 class="text-uppercase font-weight-bold mb-4">company info</h6>
+          <ul class="list-unstyled mb-0">
+            <li class="mb-2"><a href="#" >about us</a></li>
+            <li class="mb-2"><a href="#">carrier</a></li>
+            <li class="mb-2"><a href="#" >we are hiring</a></li>
+            <li class="mb-2"><a href="#">blog</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+          <h6 class="text-uppercase font-weight-bold mb-4">features</h6>
+          <ul class="list-unstyled mb-0">
+            <li class="mb-2"><a href="#" class="text-muted">business marketing</a></li>
+            <li class="mb-2"><a href="#" class="text-muted">user analytic</a></li>
+            <li class="mb-2"><a href="#" class="text-muted">live chat</a></li>
+            <li class="mb-2"><a href="#" class="text-muted">unlimited support</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+          <h6 class="text-uppercase font-weight-bold mb-4">resources</h6>
+          <ul class="list-unstyled mb-0">
+            <li class="mb-2"><a href="#" class="text-muted">IOS & Android</a></li>
+            <li class="mb-2"><a href="#" class="text-muted">watch a demo</a></li>
+            <li class="mb-2"><a href="#" class="text-muted">customers</a></li>
+            <li class="mb-2"><a href="#" class="text-muted">API</a></li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Copyrights -->
+     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    <a class="text-white" href="https://mdbootstrap.com/">made with love by figmaland all rights reserverd</a>
+  </div>
+    </div>
+  </footer>
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/rangeslider.min.js"></script>
+
+
+  <script src="js/typed.js"></script>
+            <script>
+            var typed = new Typed('.typed-words', {
+            strings: ["pain"," stress"," fatigue"],
+            typeSpeed: 80,
+            backSpeed: 80,
+            backDelay: 4000,
+            startDelay: 1000,
+            loop: true,
+            showCursor: true
+            });
+            </script>
+
+  <script src="js/main.js"></script>
+  
+  </body>
+</html>
